2d931ba6ab3009457614556a02432ffc55170e6f
rodriggocode <rodriggocode@gmail.com>
2026-05-07 22:07:04 -0300

    alteracao na rota nova veresao go + server

--- app/router/routes/posts_router.go
+++ app/router/routes/posts_router.go
@@ -7,13 +7,13 @@ import (
 )
 
 func LoadPostRoutes(router *http.ServeMux) {
-	router.HandleFunc("/publicacao/criar", middlewares.Authentication(handlers.CreatePosts))
-	router.HandleFunc("/publicacoes/", middlewares.Authentication(handlers.GetAllPosts))
-	router.HandleFunc("/publicacao/{posts_id}/publicacao", middlewares.Authentication(handlers.GetIDPost))
-	router.HandleFunc("/publicacao/{posts_id}/editar", middlewares.Authentication(handlers.UpdatedPost))
-	router.HandleFunc("/publicacao/{posts_id}/excluir", middlewares.Authentication(handlers.DeletePost))
+	router.HandleFunc("POST /publicacao/criar", middlewares.Authentication(handlers.CreatePosts))
+	router.HandleFunc("GET /publicacoes/", middlewares.Authentication(handlers.GetAllPosts))
+	router.HandleFunc("GET /publicacao/{posts_id}/publicacao", middlewares.Authentication(handlers.GetIDPost))
+	router.HandleFunc("PUT /publicacao/{posts_id}/editar", middlewares.Authentication(handlers.UpdatedPost))
+	router.HandleFunc("DELETE /publicacao/{posts_id}/excluir", middlewares.Authentication(handlers.DeletePost))
 
-	router.HandleFunc("/publicacao/{user_id}/usuarios", middlewares.Authentication(handlers.GetAllPostsUser))
-	router.HandleFunc("/publicacoes/{posts_id}/curtir", middlewares.Authentication(handlers.LikePosts))
-	router.HandleFunc("/publicacao/{posts_id}/descurtir", middlewares.Authentication(handlers.Unliked))
+	router.HandleFunc("GET /publicacao/{user_id}/usuarios", middlewares.Authentication(handlers.GetAllPostsUser))
+	router.HandleFunc("GET /publicacoes/{posts_id}/curtir", middlewares.Authentication(handlers.LikePosts))
+	router.HandleFunc("POST /publicacao/{posts_id}/descurtir", middlewares.Authentication(handlers.Unliked))
 }
